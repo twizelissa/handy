be826595a474ffc8129debef165dfd57e557b11d
Twizeyimana elissa <twizelissa@gmail.com>
2022-06-10 21:26:16 +0200

    adding images

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePressed = () => {
+    alert("Stop! Stop! Stop! you are not gonna do me like that man");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2} onPress={handlePressed}>
+        Hello all devs
+      </Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <Image source={{ uri: "https://picsum.photos/200/300" }} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
